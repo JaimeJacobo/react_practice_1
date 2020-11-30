8199c0efafe4f3dcb2d689955a93c7b2379d37ec
Jaime Jacobo <jaimejacobordv@gmail.com>
2020-11-30 12:19:33 +0100

    adding multiple props to same component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css';
 import Header from './components/Header'
 import Greet from './components/Greet'
+import Card from './components/Card'
 
 class App extends React.Component {
 
@@ -9,6 +10,18 @@ class App extends React.Component {
     username: "jaimejaime",
     email: "jaime@jaime.com",
     avatar: "https://cdn.fastly.picmonkey.com/contentful/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=800&q=70"
+  }
+
+  user1 = {
+    username: "pedropedro",
+    email: "pedro@pedro.com",
+    avatar: "https://cdn3.f-cdn.com/contestentries/1376995/30494909/5b566bc71d308_thumb900.jpg"
+  }
+
+  user2 = {
+    username: "pablopablo",
+    email: "pablo@pablo.com",
+    avatar: "https://i.redd.it/6onq25y0sh311.jpg"
   }
 
   checkForAvatar = ()=>{
@@ -24,8 +37,11 @@ class App extends React.Component {
       <div className="App">
         <Header />
         <Greet email={this.user.email} username={this.user.username}/>
-        {this.checkForAvatar()}
-        <p>{this.user.username === 'jaimejacobo' ? 'Welcome teacher!!!' : 'Welcome student'}</p>
+        {/* {this.checkForAvatar()} */}
+        {/* <p>{this.user.username === 'jaimejacobo' ? 'Welcome teacher!!!' : 'Welcome student'}</p> */}
+        <Card username={this.user.username} source={this.user.avatar}/>
+        <Card username={this.user1.username} source={this.user1.avatar}/>
+        <Card username={this.user2.username} source={this.user2.avatar}/>
       </div>
     );
   }
